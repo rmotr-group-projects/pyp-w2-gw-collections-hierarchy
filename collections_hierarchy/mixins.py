a93b78c551a50e67891e4139dce9454631e578f9
Rose <naha.hana@gmail.com>
2016-05-28 16:02:39 +0000

    first commit to test locally as tests cause memory failure in c9

--- collections_hierarchy/mixins.py
+++ collections_hierarchy/mixins.py
@@ -1,14 +1,26 @@
 class ComparableMixin(object):
+    #Implements comparison functionality. Common for List and Dict
     def __eq__(self, other):
-        pass
+        s = self.DATA_ATTR_NAME #Get the name of the property that stores the data
+        data1 = getattr(self, s) #Get the value of the property that stores the data
+        data2 = getattr(other, s)
+        
+        if type(data1) == type(data2):
+            return data1 == data2
+        
     def __ne__(self, other):
-        # Relies in __eq__
-        pass
-
+        if not self.__eq__(other):
+            return True
 
 class SequenceMixin(object):
     def __iter__(self):
-        pass
+        data1 = getattr(self, self.DATA_ATTR_NAME)
+        for d in data1:
+            while d:
+                if type(data1) is list:
+                    yield d
+                elif type(data1) is dict:
+                    yield data1[d]
 
     def __next__(self):
         """This method will rely on the get_elements() method of the
@@ -16,15 +28,17 @@ class SequenceMixin(object):
         """
         if not hasattr(self, 'get_elements'):
             raise ValueError("get_elements method not found")
-        # Keep writing your code here
+        self.get_elements()
         raise NotImplementedError()
 
     next = __next__
 
     def __len__(self):
-        # Will rely on the iterator
-        # can't do len(self.data)
-        pass
+        # uses the iterator to provide a count of the items
+        c = 0
+        for d in self:
+            c += 1
+            return c
 
     def __getitem__(self, key):
         pass
@@ -42,13 +56,19 @@ class SequenceMixin(object):
 
 class RepresentableMixin(object):
     def __repr__(self):
-        # Will rely on the iterator or __str__
-        pass
+        return self.__str__()
 
     def __str__(self):
         # Will rely on the iterator
-        pass
-
+        # uses the iterator to return a list for formatting
+        l = ",".join(map(str, self))
+        s = self.DATA_ATTR_NAME
+        
+        if type(getattr(self, s)) is list:
+            return '{}{}{}'.format('[', l, ']')
+        elif type(getattr(self, s)) is dict:
+            return '{}{}{}'.format('{', l, '}')
+        
 
 class ConstructibleMixin(object):
     DATA_ATTR_NAME = 'data'
@@ -69,20 +89,25 @@ class OperableMixin(object):
 class AppendableMixin(object):
     def append(self, elem):
         # Relies on DATA_ATTR_NAME = 'data'
-        pass
-
+        s = getattr(self, self.DATA_ATTR_NAME)
+        
+        if type(s) is list:
+            s.append(elem)
 
 class HashableMixin(object):
     def keys(self):
-        pass
+        s = getattr(self, self.DATA_ATTR_NAME)
+        return [n for n in s]
 
     def values(self):
-        pass
+        s = getattr(self, self.DATA_ATTR_NAME)
+        return [s[n] for n in s]
 
     def items(self):
-        pass
-
-
+        s = getattr(self, self.DATA_ATTR_NAME)
+        return [(n,s[n]) for n in s]
+    
 class IndexableMixin(object):
     def index(self, x):
-        pass
+        s = getattr(self, self.DATA_ATTR_NAME)
+        return s[x]
